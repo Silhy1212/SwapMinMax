4a9e37d6f5323eda26870969682ca698067f7853
matej.silhan <matejsilhan12@seznam.cz>
2024-10-23 14:39:19 +0200

    hotovo

---
+++ SwapMinMax/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+SwapMinMax.SwapMinMax swapMinMax = new SwapMinMax.SwapMinMax(1,2,"a", "b", 1.1245m,2.48866m);
+
+swapMinMax.MaxInt();
+swapMinMax.MinInt();
+swapMinMax.MaxDecimal();
+swapMinMax.MinDecimal();
+swapMinMax.SwapDecimal();
+swapMinMax.SwapInt();
+swapMinMax.SwapString();
+
---
+++ SwapMinMax/SwapMinMax.cs
@@ -0,0 +1,101 @@
+﻿namespace SwapMinMax;
+
+public class SwapMinMax
+{
+    public int a;
+    public int b;
+    public string c;
+    public string d;
+    public decimal e;
+    public decimal f;
+
+    public SwapMinMax(int a, int b, string c, string d, decimal e, decimal f)
+    {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        this.e = e;
+        this.f = f;
+    }
+
+
+    public  void SwapInt()
+    {
+        (a, b) = (b, a);
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+    }
+
+    public void SwapString()
+    {
+        (c, d) = (d, c);
+        Console.WriteLine(c);
+        Console.WriteLine(d);
+    }
+
+    public void SwapDecimal()
+    {
+        (e, f) = (f, e);
+        Console.WriteLine(e);
+        Console.WriteLine(f);
+    }
+
+    public void MaxInt()
+    {
+        int max;
+        if (a > b)
+        {
+            max = a;
+            
+        }
+        else
+        {
+            max = b;
+        }
+        Console.WriteLine(max);
+    }
+    public void MaxDecimal()
+    {
+        decimal max;
+        if (e > f)
+        {
+            max = e;
+        }
+        else
+        {
+            max = f;
+        }
+        Console.WriteLine(max);
+    }
+
+    public void MinInt()
+    {
+        int min;
+        if (a < b)
+        {
+            min = a;
+        }
+        else
+        {
+            min = b;
+        }
+        Console.WriteLine(min);
+    }
+    public void MinDecimal()
+    {
+        decimal min;
+        if (e < f)
+        {
+            min = e;
+        }
+        else
+        {
+            min = f;
+        }
+        Console.WriteLine(min);
+
+    }
+    
+    
+}
